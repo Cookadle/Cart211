5c884ff5c3021c67ad3f7ecd60e8dfa11736f3ca
Cokadle <jeany.corrius@hotmail.com>
2025-11-16 17:26:35 -0500

    finished designingthe captcha

--- CART 211/Surveillance Cap/script.js
+++ CART 211/Surveillance Cap/script.js
@@ -1,27 +1,26 @@
 let captcha;
-function generate() {
-    document.getElementById("user-input").value = ""; // <-- this is the broken line
 
+function generate() {
     captcha = document.getElementById("image");
     let uniquechar = "";
-
     const randomchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (let i = 1; i < 5; i++) {
-        uniquechar += randomchar.charAt(Math.random() * randomchar.length);
+    for (let i = 0; i < 5; i++) {
+        uniquechar += randomchar.charAt(Math.floor(Math.random() * randomchar.length));
     }
 
     captcha.innerHTML = uniquechar;
+    document.getElementById("user-input").value = "";
 }
 
 function printmsg() {
     const user_input = document.getElementById("user-input").value;
 
-    if (user_input == captcha.innerHTML) {
-        document.getElementById("key").innerHTML = "Matched";
+    if (user_input === captcha.innerHTML) {
+        document.getElementById("key").innerHTML = "Matched ✅";
         generate();
     } else {
-        document.getElementById("key").innerHTML = "Not Matched";
+        document.getElementById("key").innerHTML = "Not Matched ❌";
         generate();
     }
 }
